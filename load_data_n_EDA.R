c4b8cde1df351668fb1732e020944b2c5df169d3
shenshiwen <shenshiwen0916@gmail.com>
2020-02-18 22:55:39 -0500

    Update EDA code to draw plots with EDA_Plot_1 function.

--- load_data_n_EDA.R
+++ load_data_n_EDA.R
@@ -10,6 +10,9 @@ library(tidyverse)
 library(data.table)
 library(ggpubr)
 
+## source functions
+source("functions.R")
+
 ## set working directory
 setwd("D:/Dropbox/coronavirus/covid_2019")
 
@@ -67,95 +70,8 @@ dat_inc[,count_inc_percent := ifelse(count_last_day != 0, 100 * count_inc / coun
 dat_inc$count <- NULL
 dat_inc$count_last_day <- NULL
 
-#### EDA Start ####
-## plot cumulative counts
-ps <- "Japan"
-
-# confirm
-gg_confirm <- ggplot(dat[province_state == ps & type == "confirm"], aes(x = time, y = count)) +
-  geom_point(aes(size = count), color = "blue1", show.legend = FALSE) +
-  geom_line(linetype = "dashed", color = "blue1") +
-  scale_x_date(date_minor_breaks = "1 day") + 
-  labs(title = paste0(ps, " Cumulative Confirmed Count"))
-# plot(gg_confirm)
-# death
-gg_death <- ggplot(dat[province_state == ps & type == "death"], aes(x = time, y = count)) +
-  geom_point(aes(size = count), color = "red", show.legend = FALSE) +
-  geom_line(linetype = "dashed", color = "red") +
-  scale_x_date(date_minor_breaks = "1 day") + 
-  labs(title = paste0(ps, " Cumulative Death Count"))
-# plot(gg_death)
-# recovery
-gg_recovery <- ggplot(dat[province_state == ps & type == "recovery"], aes(x = time, y = count)) +
-  geom_point(aes(size = count), color = "cyan4", show.legend = FALSE) +
-  geom_line(linetype = "dashed", color = "cyan4") + 
-  scale_x_date(date_minor_breaks = "1 day") + 
-  labs(title = paste0(ps, " Cumulative Recovery Count"))
-# plot(gg_recovery)
-
-## plot incremental count changes
-# confirm
-gg_inc_confirm <- ggplot(dat_inc[province_state == ps & type == "confirm"], aes(x = time, y = count_inc)) +
-  geom_point(aes(size = count_inc), color = "blue1", show.legend = FALSE) +
-  geom_line(linetype = "dashed", color = "blue1") +
-  scale_x_date(date_minor_breaks = "1 day") + 
-  labs(title = paste0(ps, " Confirmed Count Change"))
-# plot(gg_inc_confirm)
-# death
-gg_inc_death <- ggplot(dat_inc[province_state == ps & type == "death"], aes(x = time, y = count_inc)) +
-  geom_point(aes(size = count_inc), color = "red", show.legend = FALSE) +
-  geom_line(linetype = "dashed", color = "red") +
-  scale_x_date(date_minor_breaks = "1 day") + 
-  labs(title = paste0(ps, " Death Count Change"))
-# plot(gg_inc_death)
-# recovery
-gg_inc_recovery <- ggplot(dat_inc[province_state == ps & type == "recovery"], aes(x = time, y = count_inc)) +
-  geom_point(aes(size = count_inc), color = "cyan4", show.legend = FALSE) +
-  geom_line(linetype = "dashed", color = "cyan4") +
-  scale_x_date(date_minor_breaks = "1 day") + 
-  labs(title = paste0(ps, " Recovery Count Change"))
-# plot(gg_inc_recovery)
-
-## plot incremental Percentage changes
-# confirm
-gg_per_confirm <- ggplot(dat_inc[province_state == ps & type == "confirm"], aes(x = time, y = count_inc_percent)) +
-  geom_point(aes(size = count_inc_percent), color = "blue1", show.legend = FALSE) +
-  geom_line(linetype = "dashed", color = "blue1") +
-  scale_x_date(date_minor_breaks = "1 day") + 
-  labs(title = paste0(ps, " Confirmed Percent Change"))
-# plot(gg_per_confirm)
-# death
-gg_per_death <- ggplot(dat_inc[province_state == ps & type == "death"], aes(x = time, y = count_inc_percent)) +
-  geom_point(aes(size = count_inc_percent), color = "red", show.legend = FALSE) +
-  geom_line(linetype = "dashed", color = "red") +
-  scale_x_date(date_minor_breaks = "1 day") + 
-  labs(title = paste0(ps, " Death Percent Change"))
-# plot(gg_per_death)
-# recovery
-gg_per_recovery <- ggplot(dat_inc[province_state == ps & type == "recovery"], aes(x = time, y = count_inc_percent)) +
-  geom_point(aes(size = count_inc_percent), color = "cyan4", show.legend = FALSE) +
-  geom_line(linetype = "dashed", color = "cyan4") +
-  scale_x_date(date_minor_breaks = "1 day") + 
-  labs(title = paste0(ps, " Recovery Percent Change"))
-# plot(gg_per_recovery)
-
-## combine plots together
-gg_comb <- ggarrange(gg_confirm + labs(title = ""),
-                     gg_death + labs(title = ""),
-                     gg_recovery + labs(title = ""), 
-                     gg_inc_confirm + labs(title = ""),
-                     gg_inc_death + labs(title = ""),
-                     gg_inc_recovery + labs(title = ""),
-                     gg_per_confirm + labs(title = ""),
-                     gg_per_death + labs(title = ""),
-                     gg_per_recovery + labs(title = ""),
-                     labels = c("Comfirmed Cum.", "Death Cum.", "Recovery Cum.",
-                                "Comfirmed Chg", "Death Chg", "Recovery Chg",
-                                "Comfirmed Chg %", "Death Chg %", "Recovery Chg %"),
-                     ncol = 3, nrow = 3)
-
-plot(gg_comb)
-
+#### EDA plots ------------------------------
+EDA_Plot_1(province_state = "Hebei", dat, dat_inc)
 
 
 
